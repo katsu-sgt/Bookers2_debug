7be6d38ff4eb0eeeccfe3d7be9cd327c51607bc9
katsu-sgt <katsu.sgt.eng@gmail.com>
2021-06-04 05:45:13 +0000

    Finish Follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,32 +11,32 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
-  
-# active 
-  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-# passive
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
+
+
+# active
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
 # フォローしている人の一覧を取り出す followingメソッドを使えるように定義している
-  has_many :following, through: :active_relationships, source: :followed
+  has_many :followings, through: :relationships, source: :followed
+
+# passive
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 # フォロワー一覧を取り出すため
-  has_many :followers, through: :passive_relationships, source: :follower
-  
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
 #ユーザーをフォローする
-  def follow(other_user)
-    following << other_user
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
   end
-  
+
 #ユーザーをフォロー解除する
-  def unfollow(other_user)
-    active_relationships.find_by(followed_id: other_user.id).destroy
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
   end
-  
+
 #現在のユーザーがフォローしてたらtrueを返す
-  def following?(other_user)
-    following.include?(other_user)
+  def following?(user)
+    followings.include?(user)
   end
-  
-  
+
+
 end
